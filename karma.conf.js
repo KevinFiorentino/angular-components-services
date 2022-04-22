fc670200249258db24965052b17736ce62751839
Kevin <kevinfiorentino.1294@gmail.com>
2022-04-22 15:43:21 -0300

    Activo Linter

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/platzi-components-services'),
+      dir: require('path').join(__dirname, './coverage/angular-components-services'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { StoreService } from 'src/app/services/store.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, OnDestroy {
 
   public countItems = 0;
+  private sub$!: Subscription;
 
   constructor(
     private storeService: StoreService
   ) { }
 
   ngOnInit(): void {
-    this.storeService.myCart$
+    this.sub$ = this.storeService.myCart$
       .subscribe(data => {
         this.countItems = data.length;
       });
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
   }
 
 }
